7d51cd09bd5ef9423d7c8eb2cb7164438982aeb4
Celeste Morton <creneemorton@gmail.com>
2020-01-17 15:21:18 -0800

    add styling to all the FE pages

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,14 @@
 class Product < ApplicationRecord
-  scope :top_reviews, -> { where("rating > ?", 2) }
+  scope :top_reviews, -> { where(rating: 5) }
   scope :three_recent, -> { order(created_at: :desc).limit(3)}
+  scope :made_in_the_usa, -> { where(country_of_origin: "USA")}
   has_many :reviews, dependent: :destroy
   
   validates :name, presence: true
   validates :cost, presence: true
   validates :country_of_origin, presence: true
   validates_length_of :name, maximum: 100
+
   before_save(:titleize_product)
 
   private
