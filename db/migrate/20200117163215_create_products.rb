65fac1104dc7a9a0e78d4be8acc49f96786d16b6
Celeste Morton <creneemorton@gmail.com>
2020-01-17 10:48:30 -0800

    add views folder and FE files

--- db/migrate/20200117163215_create_products.rb
+++ db/migrate/20200117163215_create_products.rb
@@ -1,7 +1,6 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.column(:name, :string)
 
       t.timestamps()
     end
--- db/migrate/20200117164623_add_column_to_products.rb
+++ db/migrate/20200117164623_add_column_to_products.rb
@@ -1,7 +1,7 @@
 class AddColumnToProducts < ActiveRecord::Migration[5.2]
   def change
+    add_column(:products, :name, :string)
     add_column(:products, :cost, :integer)
     add_column(:products, :country_of_origin, :string)
-    add_column(:products, :rating, :integer)
   end
 end
